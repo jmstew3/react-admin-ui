9980b692d0048fe181dc96a8bd98e77ba28087f0
stewiebeamen <jmstew8@gmail.com>
2024-04-18 13:36:39 -0400

    Begin refactoring Menu component to use dynamic rendering for menu items

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,30 +5,17 @@ import { menu } from "../../data";
 const Menu = () => {
   return (
     <div className="menu">
-      <div className="item">
-        <span className="title">MAIN</span>
-        <Link to="/" className="listItem">
-          <img src="/home.svg" alt="" />
-          <span className="listItemTitle">Home</span>
-        </Link>
-        <Link to="./business" className="listItem">
-          <img src="/profile.svg" alt="" />
-          <span className="listItemTitle">Business</span>
-        </Link>
-      </div>
-      <div className="item">
-      <span className="title">MAIN</span>
-      <Link to="/" className="listItem">
-        <img src="/home.svg" alt="" />
-        <span className="listItemTitle">Calls</span>
-      </Link>
-      <Link to="./business" className="listItem">
-        <img src="/profile.svg" alt="" />
-        <span className="listItemTitle">Marketing</span>
-      </Link>
-      </div>
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          <Link to="/" className="listItem">
+            <img src="/home.svg" alt="" />
+            <span className="listItemTitle">Home</span>
+          </Link>
+        </div>
+      ))};
     </div>
-  )
-}
+  );
+;}
 
 export default Menu
