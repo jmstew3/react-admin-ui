9801fd4fc97f231981d1c593f15ead1c0cb45c3f
stewiebeamen <jmstew8@gmail.com>
2024-04-18 13:26:10 -0400

    Update menu component with new links and styles

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,30 +1,31 @@
 import { Link } from "react-router-dom";
 import "./menu.scss"
+import { menu } from "../../data";
 
 const Menu = () => {
   return (
     <div className="menu">
       <div className="item">
         <span className="title">MAIN</span>
-        <Link to="/">
+        <Link to="/" className="listItem">
           <img src="/home.svg" alt="" />
           <span className="listItemTitle">Home</span>
         </Link>
-        <Link to="./business">
+        <Link to="./business" className="listItem">
           <img src="/profile.svg" alt="" />
           <span className="listItemTitle">Business</span>
         </Link>
       </div>
       <div className="item">
       <span className="title">MAIN</span>
-        <Link to="/">
-          <img src="/home.svg" alt="" />
-          <span className="listItemTitle">Calls</span>
-        </Link>
-        <Link to="./business">
-          <img src="/profile.svg" alt="" />
-          <span className="listItemTitle">Marketing</span>
-        </Link>
+      <Link to="/" className="listItem">
+        <img src="/home.svg" alt="" />
+        <span className="listItemTitle">Calls</span>
+      </Link>
+      <Link to="./business" className="listItem">
+        <img src="/profile.svg" alt="" />
+        <span className="listItemTitle">Marketing</span>
+      </Link>
       </div>
     </div>
   )
---
+++ src/data.ts
@@ -0,0 +1,38 @@
+export const menu = [
+    {
+        id: 1,
+        title: "main",
+        listItems: [
+            {
+                id: 1,
+                title: "Home",
+                url: "/",
+                icon: "/home.svg"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "menu",
+        listItems: [
+            {
+                id: 1,
+                title: "Business",
+                url: "/business",
+                icon: "/profile.svg"
+            },
+            {
+                id: 2,
+                title: "Calls",
+                url: "/calls",
+                icon: "/profile.svg"
+            },
+            {
+                id: 3,
+                title: "Marketing",
+                url: "/marketing",
+                icon: "/profile.svg"
+            }
+        ]
+    }
+]
