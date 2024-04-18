79fb140b8706433477c0405b23ad57c69316801d
stewiebeamen <jmstew8@gmail.com>
2024-04-18 16:42:52 -0400

    Refactor Menu component to use dynamic rendering for menu items and update menu styles. Add TopBox component

--- src/components/TopBox/TopBox.tsx
+++ src/components/TopBox/TopBox.tsx
@@ -1,9 +1,24 @@
 import "./topBox.scss"
+import { topDealUsers } from "../../data"
 
 const TopBox = () => {
     return (
         <div className="topBox">
-            TopBox
+            <h1>Top Users</h1>
+            <div className="list">
+                {topDealUsers.map((user) => (
+                    <div className="listItem" key={user.id}>
+                        <div className="user">
+                            <img src={user.img} alt={user.name} />
+                            <div className="userDetails">
+                                <span className="username">{user.username}</span>
+                                <span className="email">{user.email}</span>
+                            </div>
+                        </div>
+                        <span className="amount">${user.amount}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/data.ts
+++ src/data.ts
@@ -60,3 +60,55 @@ export const menu = [
         ]
     }
 ]
+
+export const topDealUsers = [
+    {
+      "id": 1,
+      "img": "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Elva McDonald",
+      "email": "elva@gmail.com",
+      "amount": "3.668"
+    },
+    {
+      "id": 2,
+      "img": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Linnie Nelson",
+      "email": "linnie@gmail.com",
+      "amount": "3.256"
+    },
+    {
+      "id": 3,
+      "img": "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Brent Reeves",
+      "email": "brent@gmail.com",
+      "amount": "2.998"
+    },
+    {
+      "id": 4,
+      "img": "https://images.pexels.com/photos/34577/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Mina Holmes",
+      "email": "mina@gmail.com",
+      "amount": "5.760"
+    },
+    {
+      "id": 5,
+      "img": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Clifford Ross",
+      "email": "clifford@gmail.com",
+      "amount": "1.340"
+    },
+    {
+      "id": 6,
+      "img": "https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Nellie Alvarez",
+      "email": "nellie@gmail.com",
+      "amount": "7.222"
+    },
+    {
+      "id": 7,
+      "img": "https://images.pexels.com/photos/2526108/pexels-photo-2526108.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      "username": "Gordon Freeman",
+      "email": "gordon@gmail.com",
+      "amount": "4.987"
+    }
+  ]  
