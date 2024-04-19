446c1d58135c7e003201d5c3771a529a7415620f
stewiebeamen <jmstew8@gmail.com>
2024-04-19 10:00:36 -0400

    Refactor ChartBox component to use props for dynamic rendering and update styles

--- src/components/ChartBox/ChartBox.tsx
+++ src/components/ChartBox/ChartBox.tsx
@@ -1,33 +1,52 @@
-import { Link } from 'react-router-dom'
-import './chartBox.scss'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Link } from 'react-router-dom';
+import './chartBox.scss';
+import { LineChart, Line, ResponsiveContainer, Tooltip } from 'recharts';
 
-const ChartBox = () => {
+type Props = {
+    color: string;
+    icon: string;
+    tile: string;
+    dataKey: string;
+    number: number | string;
+    percentage: number;
+    chartData: object[];
+}
+
+const ChartBox = (props) => {
     return (
         <div className="chartBox">
             <div className="boxInfo">
                 <div className="title">
-                    <img src="/user.svg" alt="" />
-                    <span>Total Users</span>
+                    <img src={props.icon} alt="" />
+                    <span>{props.title}</span>
                 </div>
-                <h1>11.238</h1>
-                <Link to="/">View all</Link>
+                <h1>{props.number}</h1>
+                <Link to="/" style={{color: props.color}}>View all</Link>
             </div>
             <div className="chartInfo">
                 <div className="chart">
-                    <ResponsiveContainer width="100%" height="100%">
-                        <LineChart width={300} height={100} data={data}>
+                    <ResponsiveContainer width="99%" height="100%">
+                        <LineChart data={props.chartData}>
+                            <Tooltip
+                            contentStyle={{ background:"none", border:"none" }}
+                            labelStyle={{ display:"none" }}
+                            position={{ x: 40, y: 90 }}
+                            />
                             <Line
                                 type="monotone"
-                                dataKey="pv"
-                                stroke="#8884d8"
+                                dataKey={props.dataKey}
+                                stroke={props.color}
                                 strokeWidth={2}
                             />
                         </LineChart>
                     </ResponsiveContainer>
                 </div>
-                <div className="text">
-                    <span className="percentage"></span>
+                <div className="texts">
+                    <span
+                        className="percentage"
+                        style={{color: props.percentage < 0 ? "tomato" : "limegreen"}}
+                    >
+                        {props.percentage}%</span>
                     <span className="duration">this month</span>
                 </div>
             </div>
