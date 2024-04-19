7264513878fab46fb5701f33d30aea40915264c7
stewiebeamen <jmstew8@gmail.com>
2024-04-19 13:43:26 -0400

    Update BarChartBox component to use props for dynamic rendering and update styles

--- src/components/BarChartBox/BarChartBox.tsx
+++ src/components/BarChartBox/BarChartBox.tsx
@@ -3,11 +3,11 @@ import "./barChartBox.scss"
 const barChartBox = (props) => {
   return (
     <div className="barChartBox">
-      <h1></h1>
+      <h1>{props.title}</h1>
       <div className="chart">
         <ResponsiveContainer width="100%" height="100%">
-            <BarChart width={150} height={40} data={data}>
-                <Bar dataKey="uv" fill="#8884d8" />
+            <BarChart width={150} height={40} data={props.chartData}>
+                <Bar dataKey={props.dataKey} fill={props.color} />
             </BarChart>
         </ResponsiveContainer>
       </div>
