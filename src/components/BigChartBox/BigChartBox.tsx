ed5db971ac01d098005f3c606ade2d2a5a878d5c
stewiebeamen <jmstew8@gmail.com>
2024-04-21 09:58:46 -0400

    Create BigChartBox component with BigChartBox.tsx and bigChartBox.scss and add component to Home page

---
+++ src/components/BigChartBox/BigChartBox.tsx
@@ -0,0 +1,11 @@
+import bigChartBox from './bigChartBox.scss'
+
+function BigChartBox() {
+  return (
+    <div className="bigChartBox">
+      BigChartBox
+    </div>
+  )
+}
+
+export default BigChartBox
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import TopBox from "../../components/TopBox/TopBox"
 import ChartBox from "../../components/ChartBox/ChartBox"
 import PieChartBox from "../../components/PieChartBox/PieChartBox"
 import BarChartBox from "../../components/BarChartBox/BarChartBox"
+import BigChartBox from "../../components/BigChartBox/BigChartBox"
 import { chartBoxUser, chartBoxProduct, chartBoxRevenue, chartBoxConversion, barChartBoxVisit, barChartBoxRevenue, pieChartBoxData} from "../../data"
 import "./home.scss"
 
@@ -16,7 +17,7 @@ const Home = () => {
       <div className="box box4"><PieChartBox {...pieChartBoxData}/></div>
       <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
-      <div className="box box7">Box7</div>
+      <div className="box box7"><BigChartBox /></div>
       <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
       <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
     </div>
