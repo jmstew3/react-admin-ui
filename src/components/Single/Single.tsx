f0b0dc576a63516edf9073f4d3436a80630850fc
stewiebeamen <jmstew8@gmail.com>
2024-04-22 18:12:46 -0400

    Refactor Single component styles in single.scss, including grid layout and image styling

--- src/components/Single/Single.tsx
+++ src/components/Single/Single.tsx
@@ -9,52 +9,17 @@ import {
 import { Tooltip } from "@mui/material";
 import "./single.scss";
 
-
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+type Props = {
+  id: number;
+  img?: string;
+  title: string;
+  info: object;
+  chart?:{
+    dataKeys:{ name: string; color: string };
+    data: object[];
+  };
+  activities?:{ time: string; text: string };
+};
 
 const Single = () => {
   return (
@@ -62,7 +27,10 @@ const Single = () => {
       <div className="view">
         <div className="info">
           <div className="topInfo">
-            <img src="../../public/Images/solo-female-1-ai.png" alt="" />
+            {props.img && <img
+              src="../../public/Images/solo-female-1-ai.png"
+              alt=""
+            />}
             <h1>Jane Doe</h1>
             <button>Update</button>
           </div>
