01a86c24c480a5fd0c1baf5492c2281fa4ae093e
stewiebeamen <jmstew8@gmail.com>
2024-04-22 11:41:34 -0400

    Refactor Add component to include form fields and handle form submission with onSubmit event in Add.tsx

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -2,29 +2,42 @@ import { GridColDef } from "@mui/x-data-grid";
 import "./add.scss";
 
 type Props = {
-    slug: string;
-    columns: GridColDef[];
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const Add = (props: Props) => {
+    
+  const handleSubmit = (e: React.FormEventHandler<HTMLFormElement>) => {
+    // prevent the form from refreshing the page
+    e.preventDefault();
+
+    // add new item
+    // axios.post(`/api/${slug}s` {})
+  };
+
   return (
     <div className="add">
       <div className="modal">
-        <span className="close" onClick={()=>props.setOpen(false)}>X</span>
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
         <h1>Add new {props.slug}</h1>
-        <form>
-            {props.columns.filter(item=>item.field !== "id" && item.field !== "img")
-            .map(column =>
-                <div className="item">
-                    <label>{column.headerName}</label>
-                    <input type={column.type} placeholder={column.field} />
-                </div>
-            )}
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
         </form>
+        <button>Send</button>
       </div>
     </div>
-  )
+  );
 };
 
-export default Add
+export default Add;
