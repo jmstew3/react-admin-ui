1a8fba454692391d3e7a35d88af90f9cf55241cd
stewiebeamen <jmstew8@gmail.com>
2024-05-14 11:42:19 -0400

    chore: Refactor TombStoneBox component and update styles in Home page

--- src/components/TombstoneBox/TombStoneBox.tsx
+++ src/components/TombstoneBox/TombStoneBox.tsx
@@ -1,47 +1,50 @@
-import React, { useContext } from 'react';
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 import { DateContext } from "../../contexts/DateContext";
 import "./tombStoneBox.scss";
 
 const TombStoneBox = () => {
-    const { jobDetails, dateRange,setDateRange } = useContext(DateContext);
-    const handleDateChange = (newFromDate: string, newToDate: string) => {
-        setDateRange({ fromDate: newFromDate, toDate: newToDate});
-    };
-    
+  const { jobDetails } = useContext(DateContext);
+
+  const tombstoneData = [
+    {
+      title: "Total Revenue",
+      value: jobDetails
+        ? `$${jobDetails.totalRevenue.toLocaleString()}`
+        : "Loading...",
+    },
+    {
+      title: "New Customers",
+      value: jobDetails
+        ? jobDetails.totalUniqueContactsCount.toLocaleString()
+        : "Loading...",
+    },
+    {
+      title: "Booked Jobs",
+      value: jobDetails
+        ? jobDetails.totalJobsBooked.toLocaleString()
+        : "Loading...",
+    },
+    {
+      title: "Completed Jobs",
+      value: jobDetails
+        ? jobDetails.totalCompletedJobs.toLocaleString()
+        : "Loading...",
+    },
+    {
+      title: "Booking Rate",
+      value: jobDetails ? `${jobDetails.bookingRate}%` : "Loading...",
+    },
+  ];
+
   return (
     <div className="tombStoneBox">
-      <div className="tombStone tombStone1">
-        <div className="title"></div>
-        <h2>Total Revenue</h2>
-        <h3>$1,200,000</h3>
-      </div>
-      <div className="tombStone tombStone2">
-        <div className="title"></div>
-        <h2>Created Jobs</h2>
-        <h3>66,351</h3>
-      </div>
-      <div className="tombStone tombStone3">
-        <div className="title"></div>
-        <h2>Completed Jobs</h2>
-        <h3>55.3k</h3>
-      </div>
-      <div className="tombStone tombStone4">
-        <div className="title"></div>
-        <h2>Booking Rate</h2>
-        {jobDetails ? (
-            <h3>
-                {`${jobDetails.bookingRate}%`}
-            </h3>
-        ) : (
-            <span>Loading...</span>
-        )}
-      </div>
-      <div className="tombStone tombStone5">
-        <div className="title"></div>
-        <h2>New Customers</h2>
-        <h3>14,650</h3>
-      </div>
+      {tombstoneData.map((item, index) => (
+        <div key={index} className={`tombStone tombStone${index + 1}`}>
+          <div className="title"></div>
+          <h2>{item.title}</h2>
+          <h3>{item.value}</h3>
+        </div>
+      ))}
     </div>
   );
 };
